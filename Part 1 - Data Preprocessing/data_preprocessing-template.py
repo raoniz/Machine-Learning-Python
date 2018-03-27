8e122c234bd964045be9855b7485331e13e50a6c
ganya7 <30626883+ganya7@users.noreply.github.com>
2018-03-27 12:06:35 +0530

    Update data_preprocessing-template.py

--- Part 1 - Data Preprocessing/data_preprocessing-template.py
+++ Part 1 - Data Preprocessing/data_preprocessing-template.py
@@ -24,9 +24,13 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_
 #X_train = scale_X.fit_transform(X_train)
 # train set needs to be fit and transformed
 # data set needs only to be transformed
-#X_test = scale_X.transform(X_test)
+#X_test = scale_X.transform(X_test)   #feature scaling in X_test is same as X_train because scale_X was fitted to X_train
+#ie why it is important to fit object to X_train first so that X_train and X_test are scaled on same basis
 # sc_y=StandardScaler()
 # y_train=sc_y.fit_transform(y_train)
+
+# feature scaling not necessary on dependent variable y when it is case of classification like y = 0 or 1
+# but when y takes a range of values then y needs to feature scaled
 # train set needs to be fit and transformed
 # test set needs only to be transformed
 
