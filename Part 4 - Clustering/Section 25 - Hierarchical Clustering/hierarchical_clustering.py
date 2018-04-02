80fe92f8dbb59a112ebb897698d57033c3ac92ce
ganya7 <30626883+ganya7@users.noreply.github.com>
2018-04-02 22:26:23 +0530

    Add files via upload

--- Part 4 - Clustering/Section 25 - Hierarchical Clustering/hierarchical_clustering.py
+++ Part 4 - Clustering/Section 25 - Hierarchical Clustering/hierarchical_clustering.py
@@ -11,21 +11,22 @@ X = dataset.iloc[:, [3, 4]].values
 # y = dataset.iloc[:, 3].values
 
 # Splitting the dataset into the Training set and Test set
-"""from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+# """from sklearn.cross_validation import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+# """from sklearn.preprocessing import StandardScaler
+# sc_X = StandardScaler()
+# X_train = sc_X.fit_transform(X_train)
+# X_test = sc_X.transform(X_test)
+# sc_y = StandardScaler()
+# y_train = sc_y.fit_transform(y_train)"""
 
 # Using the dendrogram to find the optimal number of clusters
 import scipy.cluster.hierarchy as sch
-dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))    #linkage is the algo itself of hc,
-#method to choose clusters, ward method tries to minimize the variance within each cluster just like in k means
+
+dendrogram = sch.dendrogram(sch.linkage(X, method='ward'))  # linkage is the algo itself of hc,
+# method to choose clusters, ward method tries to minimize the variance within each cluster just like in k means
 # we were trying to minimize the sum of squares within each clust, in elbow diagram 
 plt.title('Dendrogram')
 plt.xlabel('Customers')
@@ -35,15 +36,16 @@ plt.show()
 # Fitting Hierarchical Clustering to the dataset
 # distance to do the linkage = affinity
 from sklearn.cluster import AgglomerativeClustering
-hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
+
+hc = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='ward')
 y_hc = hc.fit_predict(X)
 
 # Visualising the clusters
-plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
-plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
-plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
-plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
-plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s=100, c='red', label='Cluster 1')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s=100, c='blue', label='Cluster 2')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s=100, c='green', label='Cluster 3')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s=100, c='cyan', label='Cluster 4')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s=100, c='magenta', label='Cluster 5')
 plt.title('Clusters of customers')
 plt.xlabel('Annual Income (k$)')
 plt.ylabel('Spending Score (1-100)')
--- Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.py
+++ Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.py
@@ -6,23 +6,24 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Data Preprocessing
-dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)  #if dataset does not contain header title
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header=None)  # if dataset does not contain header title
 transactions = []
 for i in range(0, 7501):
-    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
-#apriori uses list of list ie 2d array of transactions where
-#list containing different transactions(rows containing transactions) each one put in a list(items are filled in the row of the transaction)
-#one big list containing all different transactions and each transaction is going to be a list itself
+    transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
+# apriori uses list of list ie 2d array of transactions where
+# list containing different transactions(rows containing transactions) each one put in a list(items are filled in the row of the transaction)
+# one big list containing all different transactions and each transaction is going to be a list itself
 
-#apyori requires string input format
+# apyori requires string input format
 
 # Training Apriori on the dataset
-#min support = min no of times the product must appear
-#min confidence is the no of times the rules will be correct ie if c = 0.8,
-#80% of the time the rule is correct
+# min support = min no of times the product must appear
+# min confidence is the no of times the rules will be correct ie if c = 0.8,
+# 80% of the time the rule is correct
 # lift is the upgrade or improvement of product buying with the help of rule
 from apyori import apriori
-rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+
+rules = apriori(transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2)
 
 # Visualising the results
 results = list(rules)
